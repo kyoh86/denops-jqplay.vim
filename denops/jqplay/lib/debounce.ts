8090c805c82210c9abfe32acf9688368d5f5b689
kyoh86 <me@kyoh86.dev>
2024-08-13 03:16:52 +0900

    fix debounce-with-abort

--- denops/jqplay/lib/debounce.ts
+++ denops/jqplay/lib/debounce.ts
@@ -1,5 +1,5 @@
 // deno-lint-ignore no-explicit-any
-type Abortable = (signal: AbortSignal, ...args: any) => void;
+type Abortable = (signal: AbortSignal, ...args: any) => void | Promise<void>;
 
 type ParametersWithoutSignal<T extends Abortable> = T extends
   (signal: AbortSignal, ...args: infer P) => void ? P : never;
@@ -12,8 +12,10 @@ export function debounceWithAbort<T extends Abortable>(
   func: T,
   delay: number,
 ): DebouncedFunction<T> {
-  let timeoutId: number | null = null;
-  let currentController: AbortController | null = null;
+  const subscriber = new EventTarget();
+
+  let timeoutId: ReturnType<typeof setTimeout> | null = null;
+  let running: Promise<void> | null = null;
 
   return (...args: ParametersWithoutSignal<T>): void => {
     // 以前のタイマーをクリア
@@ -22,24 +24,40 @@ export function debounceWithAbort<T extends Abortable>(
     }
 
     // 以前のリクエストをキャンセル
-    if (currentController) {
-      currentController.abort();
-    }
-
-    // 新しいAbortControllerを作成
-    currentController = new AbortController();
-    const signal = currentController.signal;
+    subscriber.dispatchEvent(new CustomEvent("abort"));
 
     // 新しいタイマーを設定
-    timeoutId = setTimeout(() => {
-      try {
-        // 非同期関数を呼び出す
-        func(signal, ...args);
-      } finally {
-        // タイマーとコントローラをリセット
-        timeoutId = null;
-        currentController = null;
+    timeoutId = setTimeout(async () => {
+      const controller = new AbortController();
+      const signal = controller.signal;
+
+      // イベントリスナーを設定
+      subscriber.addEventListener("abort", () => controller.abort(), {
+        signal,
+      });
+
+      // 前の処理が完了するのを待つ
+      if (running) {
+        await running;
       }
+
+      // 新しい処理を開始
+      running = (async () => {
+        try {
+          await func(signal, ...args);
+        } catch (error) {
+          if (error.name === "AbortError") {
+            // 中断が発生した場合はエラーを無視
+            // noop
+          } else {
+            throw error;
+          }
+        } finally {
+          // タイマーとコントローラをリセット
+          timeoutId = null;
+          running = null;
+        }
+      })();
     }, delay);
   };
 }
