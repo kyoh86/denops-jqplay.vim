94116d9130299691010b156e428a716810a0c506
kyoh86 <me@kyoh86.dev>
2024-12-29 15:47:00 +0900

    doc: fix

--- denops/jqplay/lib/jq.ts
+++ denops/jqplay/lib/jq.ts
@@ -30,31 +30,31 @@ export type Params = Partial<{
   "jq-stream": true;
   "jq-stream-errors": true;
   "jq-seq": true;
-  "jq-arg": string[];
-  "jq-argjson": string[];
-  "jq-slurpfile": string[];
-  "jq-rawfile": string[];
+  "jq-arg": Record<string, string>;
+  "jq-argjson": Record<string, string>;
+  "jq-slurpfile": Record<string, string>;
+  "jq-rawfile": Record<string, string>;
   "jq-cwd": string;
   "jq-bin": string;
 }>;
 
 export const paramsSchema = v.partial(v.object({
-  "jq-raw-input": v.literal(true), // Read each line as string instead of JSON
-  "jq-slurp": v.literal(true), // Read all inputs into an array and use it as the single input value
-  "jq-compact-output": v.literal(true), // Compact instead of pretty-printed output
-  "jq-raw-output": v.literal(true), // Output strings without escapes and quotes
-  "jq-join-output": v.literal(true), // Implies -r and output without newline after each output
-  "jq-ascii-output": v.literal(true), // Output strings by only ASCII characters using escape sequences
-  "jq-sort-keys": v.literal(true), // Sort keys of each object on output
-  "jq-tab": v.literal(true), // Use tabs for indentation
-  "jq-indent": v.number(), // Use n spaces for indentation (max 7 spaces)
-  "jq-stream": v.literal(true), // Parse the input value in streaming fashion
-  "jq-stream-errors": v.literal(true), // Implies --stream and report parse error as an array
-  "jq-seq": v.literal(true), // Parse input/output as application/json-seq
-  "jq-arg": keyValueFlagSchema, // Set $name to the string value
-  "jq-argjson": keyValueFlagSchema, // Set $name to the JSON value
-  "jq-slurpfile": keyValueFlagSchema, // Set$name to an array of JSON values read from the file
-  "jq-rawfile": keyValueFlagSchema, // Set $name to string contents of file
+  "jq-raw-input": v.literal(true),
+  "jq-slurp": v.literal(true),
+  "jq-compact-output": v.literal(true),
+  "jq-raw-output": v.literal(true),
+  "jq-join-output": v.literal(true),
+  "jq-ascii-output": v.literal(true),
+  "jq-sort-keys": v.literal(true),
+  "jq-tab": v.literal(true),
+  "jq-indent": v.number(),
+  "jq-stream": v.literal(true),
+  "jq-stream-errors": v.literal(true),
+  "jq-seq": v.literal(true),
+  "jq-arg": v.record(v.string(), v.string()),
+  "jq-argjson": v.record(v.string(), v.string()),
+  "jq-slurpfile": v.record(v.string(), v.string()),
+  "jq-rawfile": v.record(v.string(), v.string()),
   "jq-cwd": v.string(),
   "jq-bin": v.string(),
 })) satisfies Schema<Params>;
@@ -81,22 +81,22 @@ export type Flags = Partial<{
 }>;
 
 export const flagsSchema = v.partial(v.object({
-  "jq-raw-input": v.pipe(v.string(), empty), // Read each line as string instead of JSON
-  "jq-slurp": v.pipe(v.string(), empty), // Read all inputs into an array and use it as the single input value
-  "jq-compact-output": v.pipe(v.string(), empty), // Compact instead of pretty-printed output
-  "jq-raw-output": v.pipe(v.string(), empty), // Output strings without escapes and quotes
-  "jq-join-output": v.pipe(v.string(), empty), // Implies -r and output without newline after each output
-  "jq-ascii-output": v.pipe(v.string(), empty), // Output strings by only ASCII characters using escape sequences
-  "jq-sort-keys": v.pipe(v.string(), empty), // Sort keys of each object on output
-  "jq-tab": v.pipe(v.string(), empty), // Use tabs for indentation
-  "jq-indent": v.pipe(v.string(), numeric), // Use n spaces for indentation (max 7 spaces)
-  "jq-stream": v.pipe(v.string(), empty), // Parse the input value in streaming fashion
-  "jq-stream-errors": v.pipe(v.string(), empty), // Implies --stream and report parse error as an array
-  "jq-seq": v.pipe(v.string(), empty), // Parse input/output as application/json-seq
-  "jq-arg": keyValueFlagSchema, // Set $name to the string value
-  "jq-argjson": keyValueFlagSchema, // Set $name to the JSON value
-  "jq-slurpfile": keyValueFlagSchema, // Set$name to an array of JSON values read from the file
-  "jq-rawfile": keyValueFlagSchema, // Set $name to string contents of file
+  "jq-raw-input": v.pipe(v.string(), empty),
+  "jq-slurp": v.pipe(v.string(), empty),
+  "jq-compact-output": v.pipe(v.string(), empty),
+  "jq-raw-output": v.pipe(v.string(), empty),
+  "jq-join-output": v.pipe(v.string(), empty),
+  "jq-ascii-output": v.pipe(v.string(), empty),
+  "jq-sort-keys": v.pipe(v.string(), empty),
+  "jq-tab": v.pipe(v.string(), empty),
+  "jq-indent": v.pipe(v.string(), numeric),
+  "jq-stream": v.pipe(v.string(), empty),
+  "jq-stream-errors": v.pipe(v.string(), empty),
+  "jq-seq": v.pipe(v.string(), empty),
+  "jq-arg": keyValueFlagSchema,
+  "jq-argjson": keyValueFlagSchema,
+  "jq-slurpfile": keyValueFlagSchema,
+  "jq-rawfile": keyValueFlagSchema,
   "jq-cwd": v.string(),
   "jq-bin": v.string(),
 })) satisfies Schema<Flags>;
@@ -111,44 +111,67 @@ const trueToString = v.pipe(
   v.transform((x) => x ? "" : undefined),
 );
 
+const keyValueFlagToRecord = v.pipe(
+  keyValueFlagSchema,
+  v.transform((x) => {
+    const record: Record<string, string> = {};
+    for (const item of x) {
+      const [key, value] = item.split("=", 2);
+      record[key] = value;
+    }
+    return record;
+  }),
+);
+
 export const flagsToParams = v.object({
-  "jq-raw-input": stringToTrue, // Read each line as string instead of JSON
-  "jq-slurp": stringToTrue, // Read all inputs into an array and use it as the single input value
-  "jq-compact-output": stringToTrue, // Compact instead of pretty-printed output
-  "jq-raw-output": stringToTrue, // Output strings without escapes and quotes
-  "jq-join-output": stringToTrue, // Implies -r and output without newline after each output
-  "jq-ascii-output": stringToTrue, // Output strings by only ASCII characters using escape sequences
-  "jq-sort-keys": stringToTrue, // Sort keys of each object on output
-  "jq-tab": stringToTrue, // Use tabs for indentation
-  "jq-indent": v.optional(stringToNumber), // Use n spaces for indentation (max 7 spaces)
-  "jq-stream": stringToTrue, // Parse the input value in streaming fashion
-  "jq-stream-errors": stringToTrue, // Implies --stream and report parse error as an array
-  "jq-seq": stringToTrue, // Parse input/output as application/json-seq
-  "jq-arg": v.optional(keyValueFlagSchema), // Set $name to the string value
-  "jq-argjson": v.optional(keyValueFlagSchema), // Set $name to the JSON value
-  "jq-slurpfile": v.optional(keyValueFlagSchema), // Set$name to an array of JSON values read from the file
-  "jq-rawfile": v.optional(keyValueFlagSchema), // Set $name to string contents of file
+  "jq-raw-input": stringToTrue,
+  "jq-slurp": stringToTrue,
+  "jq-compact-output": stringToTrue,
+  "jq-raw-output": stringToTrue,
+  "jq-join-output": stringToTrue,
+  "jq-ascii-output": stringToTrue,
+  "jq-sort-keys": stringToTrue,
+  "jq-tab": stringToTrue,
+  "jq-indent": v.optional(stringToNumber),
+  "jq-stream": stringToTrue,
+  "jq-stream-errors": stringToTrue,
+  "jq-seq": stringToTrue,
+  "jq-arg": v.optional(keyValueFlagToRecord),
+  "jq-argjson": v.optional(keyValueFlagToRecord),
+  "jq-slurpfile": v.optional(keyValueFlagToRecord),
+  "jq-rawfile": v.optional(keyValueFlagToRecord),
   "jq-cwd": v.optional(v.string()),
   "jq-bin": v.optional(v.string()),
 }) satisfies Schema<Params>;
 
+const recordToKeyValueFlag = v.pipe(
+  v.record(v.string(), v.string()),
+  v.transform((x) => {
+    const list: string[] = [];
+    for (const [key, value] of Object.entries(x)) {
+      list.push(`${key}=${value}`);
+    }
+    return list;
+  }),
+);
+
 export const paramsToFlags = v.object({
-  "jq-raw-input": trueToString, // Read each line as string instead of JSON
-  "jq-slurp": trueToString, // Read all inputs into an array and use it as the single input value
-  "jq-compact-output": trueToString, // Compact instead of pretty-printed output
-  "jq-raw-output": trueToString, // Output strings without escapes and quotes
-  "jq-join-output": trueToString, // Implies -r and output without newline after each output
-  "jq-ascii-output": trueToString, // Output strings by only ASCII characters using escape sequences
-  "jq-sort-keys": trueToString, // Sort keys of each object on output
-  "jq-tab": trueToString, // Use tabs for indentation
-  "jq-indent": v.optional(numberToString), // Use n spaces for indentation (max 7 spaces)
-  "jq-stream": trueToString, // Parse the input value in streaming fashion
-  "jq-stream-errors": trueToString, // Implies --stream and report parse error as an array
-  "jq-seq": trueToString, // Parse input/output as application/json-seq
-  "jq-arg": v.optional(keyValueFlagSchema), // Set $name to the string value
-  "jq-argjson": v.optional(keyValueFlagSchema), // Set $name to the JSON value
-  "jq-slurpfile": v.optional(keyValueFlagSchema), // Set$name to an array of JSON values read from the file
-  "jq-rawfile": v.optional(keyValueFlagSchema), // Set $name to string contents of file
+  "jq-raw-input": trueToString,
+  "jq-slurp": trueToString,
+  "jq-compact-output": trueToString,
+  "jq-raw-output": trueToString,
+  "jq-join-output": trueToString,
+  "jq-ascii-output": trueToString,
+  "jq-sort-keys": trueToString,
+  "jq-tab": trueToString,
+  "jq-indent": v.optional(numberToString),
+  "jq-stream": trueToString,
+  "jq-stream-errors": trueToString,
+  "jq-seq": trueToString,
+  "jq-arg": v.optional(recordToKeyValueFlag),
+  "jq-argjson": v.optional(recordToKeyValueFlag),
+  "jq-slurpfile": v.optional(recordToKeyValueFlag),
+  "jq-rawfile": v.optional(recordToKeyValueFlag),
   "jq-cwd": v.optional(v.string()),
   "jq-bin": v.optional(v.string()),
 }) satisfies Schema<Flags>;
