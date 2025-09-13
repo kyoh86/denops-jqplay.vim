5afe1610be91b4fc6c89ca0494b70c4969bb2c88
kyoh86 <me@kyoh86.dev>
2025-09-14 02:57:01 +0900

    Update dependencies

--- denops/jqplay/kind/buffer/handler.ts
+++ denops/jqplay/kind/buffer/handler.ts
@@ -1,8 +1,8 @@
-import type { Denops } from "jsr:@denops/std@7.6.0";
+import type { Denops } from "jsr:@denops/std@8.0.0";
 import type { Buffer } from "jsr:@kyoh86/denops-router@0.5.0";
-import * as fn from "jsr:@denops/std@7.6.0/function";
+import * as fn from "jsr:@denops/std@8.0.0/function";
 import * as v from "jsr:@valibot/valibot@1.1.0";
-import { TextLineStream } from "jsr:@std/streams@1.0.10";
+import { TextLineStream } from "jsr:@std/streams@1.0.12";
 
 import {
   BufferWritingStream,
